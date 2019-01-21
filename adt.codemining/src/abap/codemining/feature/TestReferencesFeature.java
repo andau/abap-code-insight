aef374234c896129c754083de78f99be63837dd4
andau <andau@email.com>
2019-01-21 21:07:53 +0100

    test references feature

---
+++ adt.codemining/src/abap/codemining/feature/TestReferencesFeature.java
@@ -0,0 +1,15 @@
+package abap.codemining.feature;
+
+public class TestReferencesFeature {
+
+	private final boolean isActive;
+
+	public TestReferencesFeature(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+}
